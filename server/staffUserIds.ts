23e3d3f2a2ee7c446fc5311fea663a7705ad038b
south-central1 <unkowndd00@gmail.com>
2025-12-03 20:00:57 +0100

    Update staffUserIds.ts

--- server/staffUserIds.ts
+++ server/staffUserIds.ts
@@ -4,6 +4,10 @@ export const STAFF_USER_IDS = [
   '1418678840838062144', // owner
   '970654818521722881', // Mr Pain
   '944316456173199441', // Murdaa
+  '1378750490707230802',  // R8
+  '1383396153101652079',  // Denish
+  '1202626388646174723',  // Miskoo
+  '1338399359481679937',  // MRtaketheRisk
 ];
 
 export function isStaffUser(userId: string): boolean {
